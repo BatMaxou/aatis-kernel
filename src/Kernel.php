49d2990bfe125364b9d11edf55586e020e9c8bcb
Maxime <maxime@maximebatista.lycee@gmail.com>
2023-12-04 19:39:58 +0100

    :bookmark: Env gestion

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,14 +10,17 @@ class Kernel
 {
     public function handle(): void
     {
-        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../../../', ['.env', '.env.local'], false);
+        $server = $_SERVER;
+
+        $dotenv = Dotenv::createImmutable($server['DOCUMENT_ROOT'].'../../', ['.env', '.env.local'], false);
         $dotenv->load();
 
-        $ctx = [
-            'env' => $_ENV['APP_ENV'],
-        ];
+        $ctx = array_merge(
+            array_diff_key($_SERVER, $server),
+            $server['DOCUMENT_ROOT'] ? ['APP_DOCUMENT_ROOT' => $server['DOCUMENT_ROOT']] : []
+        );
 
-        $container = (new ContainerBuilder($ctx, $_ENV['DOCUMENT_ROOT'] . '/../src'))->build();
+        $container = (new ContainerBuilder($ctx))->build();
 
         /** @var Router $router */
         $router = $container->get(Router::class);
